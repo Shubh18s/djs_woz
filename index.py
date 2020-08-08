124081a5188f6570065394b66bf76f77c730f090
Shubhdeep Singh <shubhdeep.singh@anz.com>
2020-08-08 17:24:08 +1000

    updated controller to try and send response

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 # /index.py
-from flask import Flask, request, jsonify, render_template, redirect, url_for, g
+from flask import Flask, request, jsonify, render_template, redirect, url_for, session, current_app
 import os
 import dialogflow
 import requests
@@ -29,26 +29,40 @@ bootstrap = Bootstrap(app)
 user_query="Listening..."
 wizard_response="No response"
 
+
+cache = {}
+
 class NameForm(Form):
     name = StringField('Enter Response...', validators=[Required()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    #cache['user_query']="Listening..."
+    #cache['wizard_response']="No response"
+
+    global user_query
+    global wizard_response
+    user_query="Listening..."
+    wizard_response="No response"
+
     name=None
     form=NameForm()
     if form.validate_on_submit():
         name=form.name.data
         form.name.data=''
+
+
         #request.wizard_response=form.name.data
     #return render_template('index.html')
     # if g.query:
     #     return render_template('first.html', form=form, name=name, query=g.query)
     # else:
     return render_template('first.html', form=form, name=name, query=user_query)
+    #return render_template('first.html', query=cache['user_query'])
 
-def getResponseFromWizard(user_query=""):
-    return redirect(url_for('/', query=user_query))
+# def getResponseFromWizard(user_query=""):
+#     return redirect(url_for('/', query=cache['user_query']))
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -57,8 +71,9 @@ def webhook():
     
     #return redirect(url_for('index'), query=query)
     global user_query
+    global wizard_response
     #user_query=query
-    #time.sleep(2)
+    time.sleep(2)
     if(user_query=="Listening..."):
         user_query=query
         tempResp = "Did you say, "+query+"?"
@@ -69,12 +84,15 @@ def webhook():
         return jsonify(reply)
     else:
         time.sleep(2)
-        user_query="Listening..."
+        #user_query="Listening..."
+        
         reply = {
             "fulfillmentText": wizard_response,
         }
         # global wizard_response
-        # wizard_response="No response"
+        user_query="Listening..."
+        
+        wizard_response="No response"
         return jsonify(reply)
         
     #wizard_response=""
@@ -152,6 +170,9 @@ def send_message():
 def send_response():
     global wizard_response
     wizard_response=request.form['response']
+    return wizard_response
+    #wizard_response=request.form['response']
+    #return cache['wizard_response']
 
 @app.errorhandler(404)
 def page_not_found(e):
