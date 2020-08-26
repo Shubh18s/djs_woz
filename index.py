61688396667f7c903d04254ce8d07a2e5df56f98
Shubhdeep Singh <shubhdeep.singh@anz.com>
2020-08-27 02:18:31 +1000

    database save and fetch working at both ends from the user side and the wizard. Now need to send response by executing the intent.

--- index.py
+++ index.py
@@ -52,150 +52,47 @@ def index():
 def webhook():
     data = request.get_json(silent=True)
     query = data['queryResult']['queryText']
-    
-
-# Correct syntax  - db.query(models.UserQuery).all()
-# query=db.query(models.UserQuery).order_by(models.UserQuery.id.desc()).limit(1) 
-
-
-    #conn = engine.connect()
-    #reply_diag=""
-    # diag_prev_req = db.query(models.UserQuery).order_by(models.UserQuery.id.desc()).first()
-    # reply = {
-    #     "fulfillmentText": diag_prev_req,
-    # }
-    # if diag_prev_req:
-    #     if(diag_prev_req.wizard_response=="No Response"):
-    #         reply_diag = "Please hold!! We're still getting the response"
-    #     else:
-    #         diag_new_req = models.UserQuery(user_request=query)
-    #         db.add(diag_new_req)
-    #         reply_diag = "Preparing response..."
-    
-    # if cache.get("user_utterance"):
-    #     reply = {
-    #         "fulfillmentText": "Please hold for previous response.",
-    #     }
-    # else:
-    #     cache.set("user_utterance", query)
-    #     reply = {
-    #         "fulfillmentText": "Preparing response. Please hold.",
-    #     }    
-
     global user_utter
     
     #db_resp = db.query(models.UserQuery).order_by(models.UserQuery.id.desc()).limit(1)
-    try:
-        user_utterance = db.query(models.UserQuery.user_request).order_by(models.UserQuery.id.desc()).first().user_request
-        try:
-            wizard_utterance = db.query(models.UserQuery.user_request).order_by(models.UserQuery.id.desc()).first().wizard_response
-            
+    user_utterance = db.query(models.UserQuery).order_by(models.UserQuery.id.desc()).first().user_request
+    wizard_utterance = db.query(models.UserQuery).order_by(models.UserQuery.id.desc()).first().wizard_response
 
-
-            #[[[new record]]] (1,1)
-            user_utter = query
-            new_user_utr = models.UserQuery(user_request=query)
-            db.add(new_user_utr)
-            db.commit()
-
-            # Update last record query here above this
-            reply = {
-                #"fulfillmentText": db_resp,
-                "fulfillmentText": "Preparing response. Please hold.",
-            }
-        except:
-
-            #[[[Nothing happens]]] (1,0)
-
-            reply = {
-                "fulfillmentText": "Please hold for previous response.",
-            }
-    except:
-        try:
-            wizard_utterance = db.query(models.UserQuery.user_request).order_by(models.UserQuery.id.desc()).first().wizard_response
-            
-            #[[[Update last record]]] (0,1)
-            user_utter = query
-
-
-            reply = {
-                #"fulfillmentText": db_resp,
-                "fulfillmentText": "Preparing response. Please hold.",
-            }
-        except:
-
-            #[[[This will never be true, something will always be in last record]]]
-
-            reply = {
-                "fulfillmentText": "Preparing response. Please hold.",
-            }
-
-
-
-
-
+    if (user_utterance != "Listening..." and wizard_utterance != "No Response"):
+        #[[[new record]]] (1,1)
         user_utter = query
         new_user_utr = models.UserQuery(user_request=query)
         db.add(new_user_utr)
         db.commit()
+        # Add new record above this
         reply = {
-                #"fulfillmentText": db_resp,
-                "fulfillmentText": "Preparing response. Please hold.",
-            }
-    # if(db )#(user_utter == "Listening..."):
-    #     if db_resp.:#(wizard_utter == "No response"):
-    #         user_utter = query
-    #         # ins = models.UserQuery.insert()
-    #         # conn.execute(ins, user_request=query, user_sent=1)
-    #         models.UserQuery.insert(user_request=query, user_sent=1)
-    #         reply = {
-    #             #"fulfillmentText": db_resp,
-    #             "fulfillmentText": "Preparing response. Please hold.",
-    #         }
-    #     else:
-    #         reply = {
-    #             "fulfillmentText": "Please hold for previous response.",
-    #         }
-    # else:
-    #     reply = {
-    #             "fulfillmentText": "Preparing previous response. Please hold.",
-    #         }
-            
+            #"fulfillmentText": db_resp,
+            "fulfillmentText": "Preparing response. Please hold.",
+        }
+    elif (user_utterance == "Listening..." and wizard_utterance != "No Response"):
+        #[[[Update last record]]] (0,1)
+        user_utter = query
+        db.query(models.UserQuery).order_by(models.UserQuery.id.desc()).first().user_request = query
+        db.commit()
+        reply = {
+            #"fulfillmentText": db_resp,
+            "fulfillmentText": "Preparing response. Please hold.",
+        }
+    else:
+        #[[[Nothing happens]]] (1,0) 
+        reply = {
+            "fulfillmentText": "Please hold for previous response.",
+        }   
     return jsonify(reply)
 
 
-# @socketio.on('connect', namespace='/wizard_integration')
-# def wizard_connect():
-#     print('Wizard connected')
-
-# @socketio.on('disconnect', namespace='/wizard_integration')
-# def wizard_disconnect():
-#     print('Wizard disconnected')
-
-# @socketio.on('get user query', namespace='/test')
-# def get_user_query(message):
-#     emit('user query', {'data': message['data']})
-
 @app.route('/webhook', methods=['GET'])
 def renderUserQuery():
-    #diag_prev_req = db.query(models.UserQuery).order_by(models.UserQuery.id.desc()).first() 
-    
-    # if diag_prev_req:
-    #     if(diag_prev_req.wizard_response=="No Response"):
-    #         return render_template('UserQuery.html', query = diag_prev_req.user_request)
-    # else:
-    #     return render_template('UserQuery.html', query = "Listening...")
-
     if(user_utter == "Listening..."):
         return render_template('UserQuery.html', query = "")
     else:
         return render_template('UserQuery.html', query = user_utter)
-    
 
-    # if cache.get("user_utterance"):
-    #     return render_template('UserQuery.html', query = cache.get("user_utterance"))
-    # else:
-    #     return render_template('UserQuery.html', query = "Listening")
 
 
 def detect_intent_texts(project_id, session_id, text, language_code):
@@ -222,11 +119,28 @@ def send_message():
 
 @app.route('/send_response', methods=['POST'])
 def send_response():
-    global wizard_response
-    wizard_response=request.form['response']
-    return wizard_response
-    #wizard_response=request.form['response']
-    #return cache['wizard_response']
+    global user_utter
+    global wizard_utter
+    
+    user_utterance = db.query(models.UserQuery).order_by(models.UserQuery.id.desc()).first().user_request
+    wizard_utterance = db.query(models.UserQuery).order_by(models.UserQuery.id.desc()).first().wizard_response
+
+    if (user_utterance != "Listening..." and wizard_utterance != "No Response"):
+        #[[[new record]]] (1,1)
+        wizard_utter = request.form['response']
+        new_wiz_utr = models.UserQuery(wizard_response=request.form['response'])
+        db.add(new_wiz_utr)
+        db.commit()
+    elif (user_utterance != "Listening..." and wizard_utterance == "No Response"):
+        wizard_utter = request.form['response']
+        db.query(models.UserQuery).order_by(models.UserQuery.id.desc()).first().wizard_response = request.form['response']
+        db.commit()
+    else:
+        user_utter = "Can't send response. Waiting for user."
+        time.sleep(30)
+        user_utter="Listening..."
+    
+    return wizard_utter
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -237,16 +151,6 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-# @socketio.on('render query', namespace='/queryRenderSocket')
-# def render_user_utterance():
-#     emit('user utterance', {'data': 'This is user utterance'})
-
-
-#def main():
- #   print(dialogflow.SessionsClient())
-  #  print(os.getenv('DIALOGFLOW_PROJECT_ID'))
-
-# run Flask app
 if __name__ == "__main__":
     #main()
     #app.run()
